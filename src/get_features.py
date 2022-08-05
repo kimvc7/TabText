8280ff18496a39eb7c67595a29d777556b680227
kimvc7 <30934898+kimvc7@users.noreply.github.com>
2022-08-04 20:40:22 -0400

    Update get_features.py

--- src/get_features.py
+++ src/get_features.py
@@ -31,9 +31,11 @@ def create_features(ids, time_col, id_col, patients_path, feature_type, disch_in
             timed_data = pd.read_pickle(patients_path + feature_type + "/" + str(pat_id) + ".pkl")
             timed_data = timed_data.fillna(0)
             timed_data[time_col] = timed_data[time_col].dt.to_pydatetime()
-            discharge_date = disch_info[disch_info[id_col] == pat_id][time_col].dt.date.values[0]
-            timebounded_df = get_timebounded_data(timed_data, time_col, start, min(end, discharge_date))
-            features = add_patient_features(features, pat_id, timebounded_df, id_col, time_col, weight_fn)
+            discharge_info_pat = disch_info[disch_info[id_col] == pat_id]
+            if discharge_info_pat.shape[0] > 0:
+                discharge_date = discharge_info_pat[time_col].dt.date.values[0]
+                timebounded_df = get_timebounded_data(timed_data, time_col, start, min(end, discharge_date))
+                features = add_patient_features(features, pat_id, timebounded_df, id_col, time_col, weight_fn)
 
     return features
 
@@ -43,7 +45,7 @@ def add_patient_features(features, pat_id, timebounded_df, id_col, time_col, wei
     timed_df = timebounded_df#[[time_col] + feature_cols]
     all_dates = timed_df.sort_values(time_col)[time_col].dt.date.unique()
 
-    features_df = pd.DataFrame(columns = [id_col] + list(timed_df.columns))
+    features_df = pd.DataFrame(columns = [id_col, time_col] + feature_cols)
 
     index = 0
     for date in all_dates:
@@ -57,5 +59,3 @@ def add_patient_features(features, pat_id, timebounded_df, id_col, time_col, wei
         return features_df
     else:
         return pd.concat([features, features_df])
-
-
